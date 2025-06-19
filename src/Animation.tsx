35432b9b37559e9a0c932bb6ec12e6b4c5b0c8c5
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-06-18 22:27:01 -0500

    feat: cleans up animation

--- src/Animation.tsx
+++ src/Animation.tsx
@@ -3,8 +3,9 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
+gsap.registerPlugin(MotionPathPlugin);
+
 const Animation = () => {
-  gsap.registerPlugin(MotionPathPlugin);
   const container = useRef<HTMLDivElement>(null);
 
   const { contextSafe } = useGSAP({ scope: container }); // we can pass in a config object as the 1st parameter to make scoping simple
@@ -12,18 +13,13 @@ const Animation = () => {
   // ✅ wrapped in contextSafe() - animation will be cleaned up correctly
   // selector text is scoped properly to the container.
   const onClickGood = contextSafe(() => {
-    // gsap.to(".good", { rotation: 180 });
-
     gsap.to(".good", {
-      motionPath: {
-        path: [
-          { x: 0, y: 0 },
-          { x: 20, y: 0 },
-          { x: 30, y: 50 },
-          { x: 50, y: 50 },
-        ],
-        type: "cubic",
-      },
+      motionPath: [
+        { scale: 0.5, rotation: 10 },
+        { scale: 1, rotation: -10 },
+        { scale: 0.8, rotation: 3 },
+      ],
+      duration: 2,
     });
   });
 
