b24d217987dc5bdce9db104f42051b8202269c79
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-06-26 07:48:05 -0500

    updates ScrollTriggerAnimation styles

--- src/components/ScrollTriggerAnimation.tsx
+++ src/components/ScrollTriggerAnimation.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 function ScrollTriggerAnimation() {
-  const [lettersRef, setlettersRef] = useArrayRef();
-  const container = useRef<HTMLDivElement>(null);
-  const { contextSafe } = useGSAP({ scope: container });
-  const triggerRef = useRef(null);
+  const backgroundColor = "#FEF852";
+  const textPrimaryColor = "#2A2A2A";
 
   function useArrayRef() {
     const lettersRef = useRef([]);
@@ -15,7 +12,11 @@ function ScrollTriggerAnimation() {
     return [lettersRef, (ref) => ref && lettersRef.current.push(ref)];
   }
 
+  const [lettersRef, setlettersRef] = useArrayRef();
+  const triggerRef = useRef(null);
+
   gsap.registerPlugin(ScrollTrigger);
+
   const text =
     "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut.";
 
@@ -27,7 +28,7 @@ function ScrollTriggerAnimation() {
         start: "top center",
         end: "bottom 85%",
       },
-      color: "#2A2A2A",
+      color: textPrimaryColor,
       duration: 5,
       stagger: 1,
     });
@@ -37,19 +38,37 @@ function ScrollTriggerAnimation() {
   }, []);
 
   return (
-    <div ref={container}>
-      <div className="spacing-small"></div>
-      <div className="reveal">
+    <>
+      <div style={{ height: "50vh" }} />
+      <div
+        style={{
+          background: backgroundColor,
+          paddingLeft: "30%",
+          paddingRight: "10%",
+        }}
+        className="reveal"
+      >
         <div ref={triggerRef}>
           {text.split("").map((letter, index) => (
-            <span className="reveal-text" key={index} ref={setlettersRef}>
+            <span
+              className="reveal-text"
+              style={{
+                fontWeight: "800",
+                fontSize: "clamp(3rem, 10vw, 15rem)",
+                lineHeight: "clamp(3rem, 10vw, 15rem)",
+                color: backgroundColor,
+                filter: "drop-shadow(0 0 0.06rem rgb(2, 2, 2))",
+              }}
+              key={index}
+              ref={setlettersRef}
+            >
               {letter}
             </span>
           ))}
         </div>
       </div>
-      <div className="spacing"></div>
-    </div>
+      <div style={{ height: "50vh" }} />
+    </>
   );
 }
 
